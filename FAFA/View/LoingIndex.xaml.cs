52eca7d371ac968476a8695e3657ead024c72f78
ประยุทธ์ <69198341+JKTheRipperTH@users.noreply.github.com>
2022-06-30 12:53:35 -0700

    อาจจะมีการอัพเดทเร็วๆนี้
    ยังไม่สมบรูณ์

---
+++ FAFA/View/LoingIndex.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FAFA.View
+{
+    /// <summary>
+    /// Interaction logic for LoingIndex.xaml
+    /// </summary>
+    public partial class LoingIndex : Window
+    {
+        public LoingIndex()
+        {
+            InitializeComponent();
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLoing_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TESTT");
+        }
+
+    }
+}
